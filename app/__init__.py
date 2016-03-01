7cea16114105c899c430e66939e793c354b5b70f
Stephen Bunn <scbunn@sbunn.org>
2016-02-29 19:18:05 -0800

    removed flask-bootstrap/moment and added stock

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,5 @@
 from flask import Flask
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
 from config import config
-
-# TODO: remove flask-bootstrap and flask-moments and use standard
-#       implementations
-bootstrap = Bootstrap()
-moment = Moment()
 
 
 def create_app(config_name):
@@ -14,9 +7,6 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
-
-    bootstrap.init_app(app)
-    moment.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
