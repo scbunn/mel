01d19e65ea7706323aba7e9cc25eebce26072493
Stephen Bunn <scbunn@sbunn.org>
2016-03-13 16:02:45 -0700

    started initial overview page

--- app/main/views.py
+++ app/main/views.py
@@ -5,3 +5,8 @@ from . import main
 @main.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@main.route('/overview', methods=['GET'])
+def overview():
+    return render_template('overview.html')
